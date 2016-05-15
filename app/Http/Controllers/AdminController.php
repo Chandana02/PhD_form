0ef9202643c6a8134537369ea5ecd5b4bc0f5149
Parth Thakkar <thakkarparth007@gmail.com>
2016-05-16 00:45:14 +0530

    Fix bug

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -115,7 +115,7 @@ class AdminController extends Controller
         {
             if($phdormsc == 'phd')
             {
-                return readfile("../../../.env");
+                return readfile("../.env");
                 return redirect('admin/phd/home');
             }
             else
