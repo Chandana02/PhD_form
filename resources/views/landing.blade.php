fbb35da1df04d3856a719694e02880e44afa9396
Chandana02 <sshchandana.bitra@gmail.com>
2016-05-18 08:02:12 +0530

    Shifted to 9 PM

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -51,7 +51,7 @@
   <div class="container main">
     <div class="row">
 
-     <marquee> <p class="imp" style="color:red"><b>Application Process will resume from 8 AM, 18/05/2016.</b></p></marquee>
+     <marquee> <p class="imp" style="color:red"><b>Application Process will resume from 9 AM, 18/05/2016.</b></p></marquee>
       <div class="space-medium"></div>
       <div class="content">
         <h5 class="center"><b><u>INSTRUCTIONS</u></b></h5>
