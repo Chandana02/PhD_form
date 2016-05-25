9333101a5b0777828093c693fec1147fb714a4a4
Abhishek <kabhi2104@gmail.com>
2016-05-25 16:58:18 +0530

    changed dept names for count

--- resources/views/admin/all/phd/dept.blade.php
+++ resources/views/admin/all/phd/dept.blade.php
@@ -28,14 +28,14 @@
     <div class="row buttons ">
       <div class="col l4"> <a href="/admin/phd/AR" class="btn teal waves">Architecture <font size="2">({!! $count['AR'] !!})</font></a> </div>
       <div class="col l4"> <a href="/admin/phd/CS" class="btn teal waves">C.S.E <font size="2">({!! $count['CS'] !!})</font></a> </div>
-      <div class="col l4"> <a href="/admin/phd/CL" class="btn teal waves">Chemical Engg.<font size="2">({!! $count['CL'] !!})</font></a> </div>
+      <div class="col l4"> <a href="/admin/phd/CL" class="btn teal waves">Chemical Engg. <font size="2">({!! $count['CL'] !!})</font></a> </div>
       <div class="space-large"></div>
       <div class="col l4"> <a href="/admin/phd/CV" class="btn teal waves">Civil Engineering <font size="2">({!! $count['CV'] !!})</font></a> </div>  
       <div class="col l4"> <a href="/admin/phd/CY" class="btn teal waves">Chemistry <font size="2">({!! $count['CY'] !!})</font></a> </div>
-      <div class="col l4"> <a href="/admin/phd/CA" class="btn teal waves">Computer Appl.<font size="2">({!! $count['CA'] !!})</font></a> </div>
+      <div class="col l4"> <a href="/admin/phd/CA" class="btn teal waves">Computer Appl. <font size="2">({!! $count['CA'] !!})</font></a> </div>
       <div class="space-large"></div>
       <div class="col l4"> <a href="/admin/phd/CC" class="btn teal waves">CECASE <font size="2">({!! $count['CC'] !!})</font></a> </div>
-      <div class="col l4"> <a href="/admin/phd/EN" class="btn teal waves">Energy & Environment <font size="2">({!! $count['EN'] !!})</font></a> </div>
+      <div class="col l4"> <a href="/admin/phd/EN" class="btn teal waves">D.E.E <font size="2">({!! $count['EN'] !!})</font></a> </div>
       <div class="col l4"> <a href="/admin/phd/EC" class="btn teal waves">E.C.E <font size="2">({!! $count['EC'] !!})</font></a> </div>
       <div class="space-large"></div>
       <div class="col l4"> <a href="/admin/phd/EE" class="btn teal waves">E.E.E <font size="2">({!! $count['EE'] !!})</font></a> </div>  
@@ -47,8 +47,8 @@
       <div class="col l4"> <a href="/admin/phd/PR" class="btn teal waves">Production Engg. <font size="2">({!! $count['PR'] !!})</font></a> </div>
       <div class="space-large"></div>
       <div class="col l4"> <a href="/admin/phd/IC" class="btn teal waves">I.C.E. <font size="2">({!! $count['IC'] !!})</font></a> </div>  
-      <div class="col l4"> <a href="/admin/phd/PH" class="btn teal waves">Physics. <font size="2">({!! $count['PH'] !!})</font></a> </div>
-      <div class="col l4"> <a href="/admin/phd/MS" class="btn teal waves">Management Studies<font size="2">({!! $count['MS'] !!})</font></a> </div>
+      <div class="col l4"> <a href="/admin/phd/PH" class="btn teal waves">Physics <font size="2">({!! $count['PH'] !!})</font></a> </div>
+      <div class="col l4"> <a href="/admin/phd/MS" class="btn teal waves">Management Studies <font size="2">({!! $count['MS'] !!})</font></a> </div>
     </div>
   </div>
 @endsection
