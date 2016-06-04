4157e4e842084b332f5b48f2923f0336638282af
Abhishek <kabhi2104@gmail.com>
2016-06-04 16:36:16 +0530

    hod-instructions added

--- resources/views/admin/hod-instructions.blade.php
+++ resources/views/admin/hod-instructions.blade.php
@@ -1,1 +1,141 @@
-Coming soon...
+<!DOCTYPE html>
+<html>
+<head>
+
+  <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>M.S. Ph.D. Admissions - 2016, NIT-Trichy |Instructions</title>
+
+  <link rel="stylesheet" href="{{URL::asset('assets/css/style.css')}}">
+  <link rel="stylesheet" href="{{URL::asset('assets/css/materialize.min.css')}}">
+  <!-- <link rel="shortcut icon" href="{{URL::asset('assets/logo.jpg')}}"> -->
+  <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+  <script src="{{URL::asset('assets/js/jquery-2.1.1.min.js')}}"></script>
+  <script src="{{URL::asset('assets/js/materialize.min.js')}}"></script>
+</head>
+<body>
+
+   
+   <header style="height: 25vh;
+  padding: 0px;
+  margin: 0px;
+  background-image: url('{{URL::asset('assets/images/header.png')}}');
+  background-repeat: no-repeat;
+  background-position: center;
+  background-color: #004d40;
+  background-size: contain;"></header>
+
+  
+  <nav>
+    <div class="nav-wrapper ">
+      
+      <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+      <ul class="hide-on-med-and-down">
+
+        <li><a href="/admin/home">Home</a></li>
+        <li><a href="/admin/home">Ph.D./M.S. Admissions</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+      <ul class="side-nav" id="mobile-demo">
+       <li><a href="/admin/home">Home</a></li>
+        <li><a href="/admin/home">Ph.D./M.S. Admissions</a></li>
+
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+    </div>
+  </nav>
+
+  <div class="space-medium"></div>
+  <div class="container main">
+    <div class="row">
+
+     <marquee> <p class="imp" style="color:black"><b><img src="http://schoolreportcards.in/SRC-New/images/newgood.gif">For any notices & updates we request you to keep visiting this site. </b></p></marquee>
+<!--       <marquee> <p class="imp" style="color:black"><b><img src="http://schoolreportcards.in/SRC-New/images/newgood.gif"> Applicants who want to modify/edit their application form, kindly wait for the notice on this. Keep visiting the site. </b></p></marquee>-->
+
+      <div class="space-medium"></div>
+      <div class="content">
+        <h5 class="center"><b>Instructions to Login</b></h5>
+        <ul class="pad">
+          <li>1) Click here to <a href="http://admission.nitt.edu/adminlogin">login</a> into admin panel.</li>
+          <li>2) Use <b>Username</b> & <b>Password</b> issued by Dean Academic to login. </li>
+        </ul>
+        
+        <h5 class="center"><b>Upload Signature</b></h5>
+        <ul class="pad">
+          <li>Dear Admission Co-ordinator,<br>
+          	  We request you to upload your signature in PNG/JPG/JPEG format.<br>
+          	  The dimension of signature should be less that 200px wide and 150px high.<br>
+          	  (less than or equal to 200x150 px) <br>
+
+          </li>
+
+        </ul>
+
+       <h5 class="center">Select/Reject Application</b></h5>
+        <ul class="pad">
+          <li><b>Step 1:</b> To read candidates application form, click on <b>View</b> button.</li>
+          <li><b>Step 2:</b> If applicant's form is eligible for further rounds and if applicant has paid the application fee,kindly click on <b>Select</b> button.</li>
+          <li><b>Or Step 2:</b> If applicant's form is not eligible for further rounds and if applicant has not paid the application fee,kindly do not click on <b>Select</b> button.</li>
+          
+        </ul>
+
+
+       
+
+        <h5 class="center"><b>Admit Card Generation</b></h5>
+        <ul class="pad">
+          <li>Signature of HOD/Admission Coordinator will automatically get deployed to all the selected candidates admit card.</li>
+        
+        </ul>
+
+         <h5 class="center"><b><u>Other Instructions</u></b></h5>
+        <ul class="pad">
+          <li> Click Logout to logout.</li>
+          
+        </ul>
+
+      </div>
+    </div>
+
+  </div>
+  <footer class="page-footer teal darken-4">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Contact Us</h5>
+                <p class="grey-text text-lighten-4">National Institute of Technology</p>
+                <p class="grey-text text-lighten-4">Tiruchirappalli - 620015</p>
+                <p class="grey-text text-lighten-4">Tamil Nadu, INDIA</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">QuickLinks</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="https://www.nitt.edu">Institute Website</a></li>
+                  <li><a class="grey-text text-lighten-3" href="http://www.nitt.edu/home/academics/departments/">Departments</a></li>
+                  <li><a class="grey-text text-lighten-3" href="http://www.nitt.edu/home/admissions/">Admissions</a></li>
+                  <li><a class="grey-text text-lighten-3" href="http://www.nitt.edu/contact">Contact Us</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright dark">
+            <div class="container center">
+            
+
+            <a class="grey-text text-lighten-4" href="#!">Made with &hearts; by Delta Force, NIT Trichy</a>
+
+            </div>
+          </div>
+        </footer>
+            
+
+  <script type="text/javascript">
+  $(document ).ready(function(){
+    $(".button-collapse").sideNav();
+  });
+  </script>
+  
+</body>
+</html>
