0bea6993ffc583b2bc5d869e33e430f8225d9072
Abhishek <kabhi2104@gmail.com>
2016-06-10 18:53:29 +0530

    added new instructions

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -51,18 +51,20 @@
   <div class="container main">
     <div class="row">
 
-     <marquee> <p class="imp" style="color:black"><b><img src="http://schoolreportcards.in/SRC-New/images/newgood.gif">For any notices & updates we request you to keep visiting this site. </b></p></marquee>
+     <marquee> <p class="imp" style="color:black"><b><img src="http://schoolreportcards.in/SRC-New/images/newgood.gif">Last date of application submission is extended to 14th June, 2016. For any notices & updates we request you to keep visiting this site. </b></p></marquee>
       <!--<marquee> <p class="imp" style="color:black"><b><img src="http://schoolreportcards.in/SRC-New/images/newgood.gif"> Applicants resetting will resume from 7th June, 2016. We request you to mail your registration number alongwith the query to edit/modify to phdsection@nitt.edu.</b></p></marquee> -->
 
       <div class="space-medium"></div>
       <div class="content">
-        <h5 class="center"><b><u>INSTRUCTIONS</u></b></h5>
+
+        <h5 class="center"><b><u>Instructions</u></b></h5>
         <ul class="pad">
           <li>1) Please read carefully the application <a href="http://www.nitt.edu/home/MS%20and%20Ph_D%20advt%20July%202016%20(1)%20(1).pdf">brochure</a> before applying.</li>
           <li>2) Reach out to help desk numbers provided in the <a href="contact">Contact</a> in case of a query.</li>
           <li>3) Help desk is live on Admission desk on all working days (10:30 AM -4:30 PM) at NIT Trichy. </li>
         </ul>
-        <h5 class="center"><b><u>APPLICATION PROCESS</u></b></h5>
+
+        <h5 class="center"><b><u>Application Process</u></b></h5>
         <ul class="pad">
           <li><b>Stage 1</b>: Online application is now available <a href="/home">here</a> </li>
           <li><b>Stage 2</b>: Online payment of application fee through <a href="https://www.onlinesbi.com/prelogin/icollecthome.htm?corpID=88587">SB Collect.</a> </li>
@@ -70,17 +72,22 @@
 
         </ul>
 
-       <h5 class="center"><b><u><img src="http://schoolreportcards.in/SRC-New/images/newgood.gif">Shortlisting/Admit Card</u></b></h5>
+       <h5 class="center"><b><u>Shortlisting/Admit Card</u></b></h5>
         <ul class="pad">
           <li>Dear applicants, your applications are under review. Rejected applicants would be notified through e-mail.
            Shortlisted applicants would be able to download their admit card here. We request you to keep visiting this site for any further update.</li>
           
         </ul>
 
+         <h5 class="center"><img src="http://schoolreportcards.in/SRC-New/images/newgood.gif"><b><u>Important Dates</u></b></h5>
+        <ul class="pad">
+           <li>1. <b>30th June, 2016</b> Entrance test for Ph.D. </li>
+           <li>2. <b>July 1, 2016</b> Interview for Ph.D.</li>
+        </ul>
 
        
 
-        <h5 class="center"><img src="http://schoolreportcards.in/SRC-New/images/newgood.gif"><b><u>Application Resetting Process</u></b></h5>
+        <h5 class="center"><b><u>Application Resetting Process</u></b></h5>
         <ul class="pad">
           <li>Step 1. Email to <b>phdsection@nitt.edu</b> or call at Admission Desk with regitration URL and Query(for edit/modify) and request for application reset.</li>
           <li>Step 2. Admission Desk would reset your Reg. URL and approve your application to be edited.</li>
@@ -89,15 +96,15 @@
         
         </ul>
 
-         <h5 class="center"><b><u>**ATTENTION**</u></b></h5>
+         <h5 class="center"><b><u>**Attention**</u></b></h5>
         <ul class="pad">
-          <li>1. Last date of application submission is 12/06/2016.</li>
+          <li>1. Last date of application submission is 14/06/2016.</li>
           <li>2. No hardcopies are required to be sent to the institute.</li>
           <li>3. Application number(mentioned in SB collect) and Registration number(mentioned in application/e-mail) are same.</li>
           <li>4. Selection of one department is compulsory and more than one department is optional.</li>
-          <li>5. <img src="http://schoolreportcards.in/SRC-New/images/newgood.gif"><b>Image/File Upload: </b>During the application process, dimensions of the image that you upload should be less than 300X200 px and that of the signature should be less than 200X150 px and the images can only be jpeg, jpg or png.</li>
-          <li>6. <img src="http://schoolreportcards.in/SRC-New/images/newgood.gif">Category Query: OC Category & General Category are same.</li>
-          <li>7. <img src="http://schoolreportcards.in/SRC-New/images/newgood.gif">Application resetting will be live from (7th June - 11th June, 2016).</li>
+          <li>5. <b>Image/File Upload: </b>During the application process, dimensions of the image that you upload should be less than 300X200 px and that of the signature should be less than 200X150 px and the images can only be jpeg, jpg or png.</li>
+          <li>6. Category Query: OC Category & General Category are same.</li>
+          <li>7. Application resetting will be live from (7th June - 11th June, 2016).</li>
           
         </ul>
 
