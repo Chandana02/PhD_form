1d8f04f2a691bfb9b1cc681dccb6a64bb2affd40
Abhishek <kabhi2104@gmail.com>
2016-06-15 05:34:18 +0530

    added new links and content

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -51,7 +51,7 @@
   <div class="container main">
     <div class="row">
 
-     <marquee> <p class="imp" style="color:black"><b><img src="{{URL::asset('assets/images/newgood.gif')}}">Application process had stopped at 11:59 PM IST, 14th June, 2016. For any notices & updates we request you to keep visiting http://admission.nitt.edu or http://nittadmission.org/. </b></p></marquee>
+     <marquee> <p class="imp" style="color:black"><b><img src="{{URL::asset('assets/images/newgood.gif')}}">Application process had stopped at 11:59 PM IST, 14th June, 2016. For any notices & updates we request you to keep visiting </b> <a href="http://admission.nitt.edu">http://admission.nitt.edu</a> or <a href="http://nittadmission.org/">http://nittadmission.org/</a></p></marquee>
       <!--<marquee> <p class="imp" style="color:black"><b><img src="http://schoolreportcards.in/SRC-New/images/newgood.gif"> Applicants resetting will resume from 7th June, 2016. We request you to mail your registration number alongwith the query to edit/modify to phdsection@nitt.edu.</b></p></marquee> -->
 
       <div class="space-medium"></div>
@@ -75,9 +75,9 @@
        <h5 class="center"><b><u>Shortlisting/Admit Card</u></b></h5>
         <ul class="pad">
           <li>Dear applicants, <br>
-          Thank you for applications at NIT Trichy. <br>
-          Your applications are under review. Rejected applicants would be notified through e-mail.
-          Shortlisted applicants would be able to download their admit card here. We request you to keep visiting this site for any further update.</li>
+          Thank you for applying at NIT Trichy. <br>
+          Your applications are under review. Rejected applicants would be notified here.
+          Shortlisted applicants would be able to download their admit card here. We request you to keep visiting <a href="http://admission.nitt.edu">http://admission.nitt.edu</a> or <a href="http://nittadmission.org/">http://nittadmission.org/</a> for any further update.</li>
           
         </ul>
 
