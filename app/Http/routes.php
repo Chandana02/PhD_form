ed6863789dd44e770d55bafdc7934033312b4897
Abhishek <kabhi2104@gmail.com>
2016-06-30 14:17:54 +0530

    deactivated admit

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -79,9 +79,13 @@ Route::group(['middleware' => 'adminauth'], function () {
 });
 Route::get('print/{phdormsc}/{regNo}', 'AdminController@printer' );
 Route::get('admit/{phdormsc}/{regNo}/{dept}', 'AdminController@admitCard');
+
+/*
 Route::get('admit', function() {
    return view('admit');
 });
+*/
+
 Route::post('admitcard', 'ApplicationController@admit');
 Route::post('phdvalidate', 'PhdController@validated');
 Route::post('msvalidate', 'MsController@validated');
